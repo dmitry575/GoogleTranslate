b04f4b9b334302b06a221e9ab00b9ec75ef27b07
Dmitry Tarakanov <dtarakanov@swiftcom.uk>
2022-11-15 16:45:21 +0300

    clean plain text

--- src/Common/ConvertPlanText.cs
+++ src/Common/ConvertPlanText.cs
@@ -1,6 +1,176 @@
-﻿namespace GoogleTranslate.Common;
+﻿using System.Text.RegularExpressions;
+using GoogleTranslate.Common.Impl;
+using GoogleTranslate.Common.Models;
 
-    public sealed class ConvertPlanText : IConvertPlanText
+namespace GoogleTranslate.Common;
+
+public sealed class ConvertPlanText : IConvertPlanText
 {
-}
+    private const string PrefixTag = "21";
+    private const string GroupPrefixTag = "22";
 
+    private readonly char[] _listNumbers = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+    private readonly char[] _listSpaces = { ' ', '\r', '\n' };
+    
+    /// <summary>
+    /// Clean double spaces
+    /// </summary>
+    private readonly Regex _regexSpace = new Regex("[ ]{2,}", RegexOptions.None);
+
+    public ConvertResult Convert(string content)
+    {
+        var result = new ConvertResult();
+        (var clean, result.Tags) = GetClean(content);
+
+        // get groups of tags
+        (clean, result.Groups) = GetGroup(clean);
+
+        result.Content = clean;
+
+        return result;
+    }
+
+    private (string clean, Dictionary<int, string> Tags) GetClean(string content)
+    {
+        var index = 0;
+        return (content,new Dictionary<int, string>());
+    }
+
+    /// <summary>
+    /// Grouping new data
+    /// </summary>
+    private (string, Dictionary<int, string>) GetGroup(string clean)
+    {
+        var groupTags = new Dictionary<int, string>();
+        clean = clean.Trim();
+
+        // empty or space symbol
+        const int EMPTY = 0;
+
+        // current position int any tags
+        const int INSERT_TAG = 1;
+
+        // current position after close tag
+        const int AFTER_TAG_EMPTY = 2;
+
+        int status = 0;
+        int cur = 0;
+        int count = 0;
+        int index = 0;
+
+        for (int i = 0; i < clean.Length; i++)
+        {
+            switch (status)
+            {
+                case EMPTY:
+                    if (clean[i] == '[')
+                    {
+                        if (clean.Substring(i + 1, 2) == PrefixTag)
+                        {
+                            cur = i;
+                            status = INSERT_TAG;
+                            count++;
+                        }
+                    }
+
+                    break;
+
+                case INSERT_TAG:
+                    if (clean[i] == ']')
+                    {
+                        status = AFTER_TAG_EMPTY;
+                        break;
+                    }
+
+                    if (!_listNumbers.Contains(clean[i]))
+                    {
+                        count = 0;
+                        status = EMPTY;
+                    }
+
+
+                    break;
+
+                case AFTER_TAG_EMPTY:
+                    if (clean[i] == '[')
+                    {
+                        status = INSERT_TAG;
+                        count++;
+                        break;
+                    }
+
+                    if (!_listSpaces.Contains(clean[i]))
+                    {
+                        if (count > 1)
+                        {
+                            groupTags.Add(index++, clean.Substring(cur, i - cur));
+                        }
+
+                        count = 0;
+                        status = EMPTY;
+                    }
+
+                    break;
+            }
+        }
+
+        if (count > 0 && status == AFTER_TAG_EMPTY && cur > 0)
+        {
+            groupTags.Add(index, clean.Substring(cur, clean.Length - cur));
+        }
+
+        foreach (var key in groupTags.Keys.ToArray())
+        {
+            clean = clean.Replace(groupTags[key], $" [{GroupPrefixTag}{key}] \r\n");
+            groupTags[key] = _regexSpace.Replace(groupTags[key], " ");
+        }
+
+        clean = clean.Replace($".[{PrefixTag}", $". [{PrefixTag}");
+        clean = clean.Replace($"![{PrefixTag}", $"! [{PrefixTag}");
+        clean = clean.Replace($".[{PrefixTag}", $". [{PrefixTag}");
+        clean = clean.Replace($"![{PrefixTag}", $"! [{PrefixTag}");
+
+        clean = _regexSpace.Replace(clean, " ");
+
+        // need for google
+        clean = clean.Replace(" ,", ", ");
+
+        return (clean, groupTags);
+    }
+
+    public string UnConvert(string dirtyContent, Dictionary<int, string> groups, Dictionary<int, string> tags)
+    {
+        return ReplaceTagToText(
+            ConvertTags.GetUnGroup(
+                ConvertTags.CleanOnlyTags(dirtyContent, PrefixTag, GroupPrefixTag),
+                GroupPrefixTag,
+                groups),
+            tags);
+    }
+
+    private string ReplaceTagToText(string translate, Dictionary<int, string> tags)
+    {
+        var result = translate;
+        foreach (var tag in tags)
+        {
+            var key = $"{PrefixTag}{tag.Key}";
+
+            Regex regexTag = new Regex(@"\[\s*(" + key + @")\s*\]");
+            if (regexTag.IsMatch(result))
+            {
+                result = regexTag.Replace(result, "[$1]");
+            }
+
+
+            if (!result.Contains("[" + key + "]"))
+            {
+                throw new ConvertException($"not found key {key} for {tag.Value}");
+            }
+
+            result = result.Replace("[" + key + "] ", tag.Value);
+            result = result.Replace("[" + key + "]", tag.Value);
+        }
+
+        return result;
+    }
+}
---
+++ src/Common/Impl/ConvertTags.cs
@@ -0,0 +1,71 @@
+﻿using System.Text.RegularExpressions;
+
+namespace GoogleTranslate.Common.Impl;
+
+public class ConvertTags
+{
+    /// <summary>
+    /// Replace group
+    /// </summary>
+    /// <param name="translate"></param>
+    /// <param name="groupPrefix"></param>
+    /// <param name="tagsGroups"></param>
+    /// <returns></returns>
+    /// <exception cref="ConvertException"></exception>
+    public static string GetUnGroup(string translate, string groupPrefix, Dictionary<int, string> tagsGroups)
+    {
+        var result = translate;
+        foreach (var tagsGroup in tagsGroups)
+        {
+            var key = $"{groupPrefix}{tagsGroup.Key}";
+
+            Regex regexTag = new Regex(@"\[\s*(" + key + @")\s*\]");
+            if (regexTag.IsMatch(result))
+            {
+                result = regexTag.Replace(result, "[$1]");
+            }
+
+            if (!result.Contains("[" + key + "]"))
+            {
+                throw new ConvertException($"not found key [{key}] for {tagsGroup.Value}");
+            }
+
+            if (tagsGroup.Value.StartsWith("</"))
+            {
+                result = result.Replace(" [" + key + "]", tagsGroup.Value);
+            }
+            else
+            {
+                result = result.Replace("[" + key + "] ", tagsGroup.Value);
+            }
+
+            result = result.Replace("[" + key + "]", tagsGroup.Value);
+        }
+
+        return result;
+    }
+
+    /// <summary>
+    /// Clean tags in the text
+    /// </summary>
+    /// <param name="translate">Dirty text</param>
+    /// <param name="prefixTag">Prefix for tags</param>
+    /// <param name="prefixGroupTag">Prefix fro group of tags</param>
+    public static string CleanOnlyTags(string translate, string prefixTag, string prefixGroupTag)
+    {
+        string clean = translate;
+        Regex regex = new Regex(@"\[\s*(" + prefixTag + @"[0-9]+)\s*\]");
+        if (regex.IsMatch(clean))
+        {
+            clean = regex.Replace(clean, "[$1]");
+        }
+
+        Regex regexGroup = new Regex(@"\[\s*(" + prefixGroupTag + @"[0-9]+)\s*\]");
+        if (regexGroup.IsMatch(clean))
+        {
+            clean = regexGroup.Replace(clean, "[$1]");
+        }
+
+        return clean;
+    }
+}
